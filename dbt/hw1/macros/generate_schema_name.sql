888a5a8b0a178d8c419982f5d6be8ab6f13c3652
arkrasouski <arkrasouski@arortem.ru>
2026-07-01 16:11:31 +0300

    feat: добавил проверку измененных моделей

--- dbt/hw1/macros/generate_schema_name.sql
+++ dbt/hw1/macros/generate_schema_name.sql
@@ -1,7 +1,18 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-  {% if custom_schema_name is not none %}
+-- {% macro generate_schema_name(custom_schema_name, node) %}
+--   {% if custom_schema_name is not none %}
+--     {{ return(custom_schema_name) }}
+--   {% else %}
+--     {{ return(target.schema) }}
+--   {% endif %}
+-- {% endmacro %}
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+  {%- set default_schema = target.schema -%}
+  {%- if target.name == 'ci' -%}
+    {{ return('dbt_ci_' ~ env_var('DBT_CI_USER', 'unknown')) }}
+  {%- elif custom_schema_name is none -%}
+    {{ return(default_schema) }}
+  {%- else -%}
     {{ return(custom_schema_name) }}
-  {% else %}
-    {{ return(target.schema) }}
-  {% endif %}
-{% endmacro %}
+  {%- endif -%}
+{%- endmacro %}
